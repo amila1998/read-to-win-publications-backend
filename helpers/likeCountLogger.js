d671902ef17793ef18e0329e37cf27b46bdccd57
amila1998 <amiladevin@gmail.com>
2023-06-26 01:14:37 +0530

    some changes & bug fix

--- helpers/likeCountLogger.js
+++ helpers/likeCountLogger.js
@@ -1,18 +1,9 @@
-const winston = require("winston");
 const fs = require("fs");
 const path = require("path");
 const logger = require("./appLogger");
 
-const likeCountLogger = ({logMessage}) => {
+const likeCountLogger = ({ logMessage, logFilePath, logsFolder }) => {
   try {
-    const timestamp = new Date()
-      .toISOString()
-      .replace(/[-:]/g, "")
-      .slice(0, -5); // Get current timestamp
-    const logFileName = `authors_likes_log_${timestamp}.log`;
-    const logsFolder = path.join(process.cwd(), "logs");
-    const logFilePath = path.join(logsFolder, logFileName);
-
     // Check if the logs folder exists, create it if it doesn't
     if (!fs.existsSync(logsFolder)) {
       fs.mkdirSync(logsFolder);
--- likeCounterTask.js
+++ likeCounterTask.js
@@ -9,6 +9,14 @@ const likeCountLogger = require("./helpers/likeCountLogger");
 
 const task = cron.schedule("*/1 * * * *", async () => {
   try {
+    const timestamp = new Date()
+      .toISOString()
+      .replace(/[-:]/g, "")
+      .slice(0, -5); // Get current timestamp
+    const logFileName = `authors_likes_log_${timestamp}.log`;
+    const logsFolder = path.join(process.cwd(), "logs");
+    const logFilePath = path.join(logsFolder, logFileName);
+
     // Fetch the like count for each author
     const authors = await User.find({ role: "author" }).select("-password");
 
@@ -38,7 +46,7 @@ const task = cron.schedule("*/1 * * * *", async () => {
 
       // Construct the log message with date, time, author details, and like count
       const logMessage = `Author: ${firstName} ${lastName}, Email: ${email}, No of Publications: ${authorPublishedBooks.length}, Like Count: ${likeCount}\n`;
-      likeCountLogger({ logMessage });
+      likeCountLogger({ logMessage, logFilePath, logsFolder });
     });
   } catch (error) {
     logger.info(error);
