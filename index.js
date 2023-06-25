f45baa1455b3a7dd46a189cb8dc2df14f9807919
amila1998 <amiladevin@gmail.com>
2023-06-25 14:04:10 +0530

    database connection created

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || process.env.DEV_PORT;
 
+// Import database connection
+require("./database/connection");
+
 // Middleware
-app.use(cors({
-    origin: '*',
+app.use(
+  cors({
+    origin: "*",
     credentials: true,
-}));
+  })
+);
 
 // Set trust proxy for secure cookies
 app.set("trust proxy", 1);
 
 // Routes
 app.get("/", (req, res) => {
-    res.send("Read to win publications backend server");
+  res.send("Read to win publications backend server");
 });
 
 // HTTP request logger
 app.listen(PORT, () => {
-    console.log(`Server is starting at ${PORT}`);
+  console.log(`Server is starting at ${PORT}`);
 });
